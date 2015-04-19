db0f4810716bc8c2a92eb35710c367348c372017
James Lee <jjangmes@gmail.com>
2015-04-19 16:48:00 +0000

    Added controllers for game, piece, and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417081559) do
+ActiveRecord::Schema.define(version: 20150419001730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pieces", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
