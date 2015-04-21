30d1fab8fd29df9f6ab5c8b1131021d1f4af8264
helloramon <a.ramon.arriola.jr@gmail.com>
2015-04-21 05:04:29 +0000

    starting pieces + params

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,6 @@
 class Game < ActiveRecord::Base
-has_many :users
-has_many :pieces
-delegate :kings, :queens, :bishops, :knights, :rooks, :pawns, to: :pieces
+	has_many :users
+	has_many :pieces
 	
 	def initialize
 		# Black pieces
