c7f226145ff23f6314cde624b029dda5c9544957
Jeremy Carey <jeremycar@gmail.com>
2015-04-21 20:16:42 -0700

    modify populate_board to associate pieces with a game, add basic tests

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'create game' do
+    game = create(:game)
+    game.populate_board
+
+    assert_equal game.pieces.count, 32, 'populate_board should create 32 total pieces'
+    assert_equal game.pieces.where(:color => 'black').count, 16, 'populate_board should create 16 black pieces'
+    assert_equal game.pieces.where(:color => 'white').count, 16, 'populate_board should create 16 white pieces'
+  end
 end
