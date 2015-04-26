39e65c16c88f9abeeb1512d2c0bb0a481a7bacca
James Lee <jjangmes@gmail.com>
2015-04-26 02:40:50 +0000

    Changed setting on Facebook to be able to log-on within local setting. And fixed a typo in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   def self.from_omniauth(auth)
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  		user.provider = auth.provideruser.uid = auth.uid
+  		user.provider = auth.provider
+      user.uid = auth.uid
   		user.email = auth.info.email
   		user.password = Devise.friendly_token[0, 20]
   	end
