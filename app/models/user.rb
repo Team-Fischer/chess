50dba4e2767d7b68258deb2fc68f0e7ec7535d5f
James Lee <jjangmes@gmail.com>
2015-04-27 08:03:57 +0000

    Fixed up some HTML and CSS to center texts. Began working on user model to be able to see players that are online

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   has_many :games
   has_many :pieces
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+
   def self.from_omniauth(auth)
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   		user.provider = auth.provider
@@ -12,6 +16,10 @@ class User < ActiveRecord::Base
   		user.email = auth.info.email
   		user.password = Devise.friendly_token[0, 20]
   	end
+  end
+
+  def online?
+    updated_at > 10.minutes.ago
   end
 
 end
