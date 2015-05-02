abf13614789f2592462f270137fb360f093edfaa
helloramon <a.ramon.arriola.jr@gmail.com>
2015-05-02 08:14:03 +0000

    test are green, following suit and made the appropriate glyph method

--- app/models/pieces/knight.rb
+++ app/models/pieces/knight.rb
@@ -1,2 +1,13 @@
 class Knight < Piece
+  def valid_move?(x_destination, y_destination)
+    x_dist = (x_coord - x_destination).abs
+    y_dist = (y_coord - y_destination).abs
+
+    (x_dist == 4 && y_dist == 1) || (x_dist == 1 && y_dist == 4)
+  end
+
+  def glyph
+    'knight'
+  end
+
 end
