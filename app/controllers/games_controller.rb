f105aa206e365b22274136abf19da319b808a2db
Harvey <hdbridges11@gmail.com>
2015-05-04 04:47:54 +0000

    added ability to list games needing another player and join them

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!, :only => [:create, :update]
+  def index
+    @games = Game.all
+  end
+  
   def new
     @game = Game.new
-    @games = Game.all
   end
 
   def show
@@ -9,13 +13,19 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = Game.create(:white_user_id => current_user)
+    @game = Game.create(:white_user_id => current_user.id)
     redirect_to game_path(@game)
   end
 
   def update
     @game = Game.find(params[:id])
-    @game.update_attributes(:black_user_id => current_user)    
+    if @game.white_user_id == current_user.id
+      flash[:alert] = "You can't play against yourself!"
+      redirect_to games_path
+    else
+      @game.update_attributes(:black_user_id => current_user.id)
+      redirect_to game_path(@game)
+    end
   end
 
   private
--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+  test "not logged in create game" do
+    post :create, { :game => { } }
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "Logged in create game" do
+    user = create(:user)
+    sign_in user
+    assert_difference 'Game.count' do
+      post :create, { :game => { :white_user_id => user.id } }
+    end
+    assert_redirected_to game_path(Game.last.id)
+  end
+  
+  # TODO: Ask Ken about how to write this test
+  # test "join not logged in" do
+  #   white_user = create(:user)
+  #   black_user = create(:user)
+  #   sign_in white_user
+  #   post :create, { :game => { :white_user_id => white_user.id } }
+
+  #   put :update, { :game => { :id => Game.last.id,
+  #                             :black_user_id => black_user.id
+  #                           }
+  #               }
+  #   assert_redirected_to new_user_session_path
+  # end
+  
+  # test "Join logged in" do
+  #   user = create(:user)
+  #   game = create(:game)
+  #   sign_in user
+  #   put :update, { :game => { :id => Game.last.id,
+  #                           :black_user_id => user.id 
+  #                         }
+  #               }
+  #   assert_redirected_to game_path(Game.last.id)             
   # end
 end
