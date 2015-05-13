e779d696efc7d74337a144fcb4aebe768acd6b2a
Harvey Bridges <hdbridges11@gmail.com>
2015-05-13 05:41:24 +0000

    indent fix

--- app/models/pieces/pawn.rb
+++ app/models/pieces/pawn.rb
@@ -3,12 +3,12 @@ class Pawn < Piece
     x_dist = (x_coord - x_destination).abs
     y_dist = (y_coord - y_destination).abs
     
-    if game.piece_at(x_destination, y_destination) && color != game.piece_at(x_destination, y_destination).color
-	    y_dist == 1 && x_dist == 1
-	elsif self.moved 
-		y_dist == 1 && x_dist == 0
-	else
-		(y_dist == 1 || y_dist == 2) && x_dist == 0
-	end
+    if game.piece_at(x_destination, y_destination) && color != game.piece_at(x_destination, y_destination).color 
+      y_dist == 1 && x_dist == 1
+    elsif self.moved 
+      y_dist == 1 && x_dist == 0
+    else
+      (y_dist == 1 || y_dist == 2) && x_dist == 0
+    end
   end
 end
