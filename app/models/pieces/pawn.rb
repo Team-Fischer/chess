956789804869ea542d14177eaafa652e04d4972d
jjangmes <jjangmes@gmail.com>
2015-05-14 03:37:27 +0000

    WIP diagonal pawn capture

--- app/models/pieces/pawn.rb
+++ app/models/pieces/pawn.rb
@@ -1,42 +1,27 @@
 class Pawn < Piece
 
-  def valid_move?(x_destination, y_destination)
-    # Jeremy removed .abs from distance calculations
-    x_dist = (x_coord - x_destination)
-    y_dist = (y_coord - y_destination)
+  def valid_move?(x_dest, y_dest)
+    x_dist = (x_dest - x_coord)
+    y_dist = (y_dest - y_coord)
 
-   #Original Code that works
-   # if self.moved
-   #    y_dist == 1 && x_dist == 0
-   # else
-   #    (y_dist == 1 || y_dist == 2) && x_dist == 0
-   # end
+    if x_dist == -1 && y_dist == -1 && game.piece_at(x_dest, y_dest)
+      
 
-   # Jeremy's suggestion
-   # Make sure to include the diagonal capture logic
-   # that Harvey added that's on master now
-   if color == 'white'
-     if moved
-       return y_dist == 1 && x_dist == 0
-     else
+    if color == 'white'
+      if moved
+        return y_dist == -1 && x_dist == 0
+      else
+        return (y_dist == -1 || y_dist == -2) && x_dist == 0
+      end
+    else
+      if moved
+        return y_dist == 1 && x_dist == 0
+      else
        return (y_dist == 1 || y_dist == 2) && x_dist == 0
-     end
-   else
-     if moved
-       return y_dist == -1 && x_dist == 0
-     else
-       return (y_dist == -1 || y_dist == -2) && x_dist == 0
-     end
-   end
-
-   # this won't work, because Game doesn't have a color
-  #  while Game.where(:color => 'white') do
-  #   if self.moved
-  #     y_dist == -1 && x_dist == 0
-  #   else
-  #     (y_dist == -1 || y_dist == -2) && x_dist == 0
-  #   end
-  #  end
+      end
+    end
+   
+#   game.piece_at(x_destination, y_destination) && color != game.piece_at(x_destination, y_destination).color
 
   end
 end
