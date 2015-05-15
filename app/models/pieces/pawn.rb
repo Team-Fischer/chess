405dd749abacdf39be47320959baf55c8655db68
Rachel Geistfeld <rachel.geistfeld@gmail.com>
2015-05-15 00:49:31 +0000

    Added TODO

--- app/models/pieces/pawn.rb
+++ app/models/pieces/pawn.rb
@@ -2,9 +2,8 @@ class Pawn < Piece
   def valid_move?(x_dest, y_dest)
     x_dist = (x_dest - x_coord)
     y_dist = (y_dest - y_coord)
-
-    # did this as elsif because the line would be too long with ||
-    if x_dist.abs == 1 && y_dist == -1 && game.piece_at(x_dest, y_dest)
+    if x_dist.abs ==  1 && y_dist == -1 && game.piece_at(x_dest, y_dest)
+    	#TODO: Refactor such that valid_move? causes no side effects
       Piece.capture(x_dest, y_dest)
       return true
     elsif x_dist.abs == 1 && y_dist == 1 && game.piece_at(x_dest, y_dest)
